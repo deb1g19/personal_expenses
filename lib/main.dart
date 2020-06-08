cfebcbd9294fa1455a711f890acdaf781e468386
deb1g19 <deb1g19@soton.ac.uk>
2020-06-08 22:34:39 +0100

    Transactions list is now initially empty

--- lib/main.dart
+++ lib/main.dart
@@ -42,12 +42,7 @@ class TransactionPage extends StatefulWidget {
 }
 
 class _TransactionPageState extends State<TransactionPage> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-        id: "t1", title: "New Shoes", amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: "t2", title: "Groceries", amount: 10.50, date: DateTime.now())
-  ];
+  final List<Transaction> _userTransactions = [];
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((element) {
